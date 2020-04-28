f2938b2df2b7db0c6992f4ef3dce1e83de7e7e0f
Luke Nihlen <luke.nihlen@gmail.com>
2020-04-28 10:13:58 -0700

    test run with new hardcoded py path

--- .travis/before-install-windows.sh
+++ .travis/before-install-windows.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
 choco install python3
-echo '======== GREP'
-grep python3 /c/ProgramData/chocolatey/logs/chocolatey.log
-echo '======== TAIL'
-tail -n100 /c/ProgramData/chocolatey/logs/chocolatey.log
-echo '======== FIND'
-find /c/ProgramData/chocolatey -name 'python*'
-echo '======== FIND TARGETDIR'
-find /c/Python38 -name '*.exe'
-exit 4
 
--- .travis/before-script-windows.sh
+++ .travis/before-script-windows.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cmake -DPYTHON_EXECUTABLE=$HOME/python3/python.exe ..
+cmake -DPYTHON_EXECUTABLE=/c/Python38/python.exe ..
 
