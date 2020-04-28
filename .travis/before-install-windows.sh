cd8947abff05a4d54e876dce7e5ff18840cc8403
Luke Nihlen <luke.nihlen@gmail.com>
2020-04-28 09:50:16 -0700

    ok its a big log

--- .travis/before-install-windows.sh
+++ .travis/before-install-windows.sh
@@ -2,6 +2,7 @@
 
 choco install python3
 ls -la /c/ProgramData/chocolatey/bin
-cat /c/ProgramData/chocolatey/logs/chocolatey.log
+grep python3 /c/ProgramData/chocolatey/logs/chocolatey.log
+tail -n100 /c/ProgramData/chocolatey/logs/chocolatey.log
 exit 4
 
