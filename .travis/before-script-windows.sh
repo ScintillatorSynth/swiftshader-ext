48e374bc168ee9740215c608c446f7d60b01da5a
Luke Nihlen <luke.nihlen@gmail.com>
2020-04-28 11:32:18 -0700

    re-enable other builds

--- .travis/before-script-windows.sh
+++ .travis/before-script-windows.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+# TODO: Brittle. Path found by case-insensitive grepping the Chocolatey install log for python3. This could easily break
+# if/when chocolatey decides to change this path. Chocolatey ignores any provided path.
 cmake -DPYTHON_EXECUTABLE=/c/Python38/python.exe ..
-
