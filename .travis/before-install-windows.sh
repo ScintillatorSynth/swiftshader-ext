773b7a8031b65500145b81f512399ae817672d60
Luke Nihlen <luke.nihlen@gmail.com>
2020-04-28 14:34:48 -0700

    try adding shasum from chocolatey

--- .travis/before-install-windows.sh
+++ .travis/before-install-windows.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-choco install python3
-
+# psutils is to pick up shasum, which for some reason is not included in git bash on Travis
+choco install python3 psutils
+which shasum
+exit 4
