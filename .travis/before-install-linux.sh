ce82a94cd10b3ba5df89c70575b5a8d219480b18
Luke Nihlen <luke.nihlen@gmail.com>
2020-07-31 11:09:27 -0700

    install gcc7

--- .travis/before-install-linux.sh
+++ .travis/before-install-linux.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
 sudo apt-get update
-sudo apt-get install build-essential clang-8 cmake
+# installing gcc-7 and g++-7 to get libstdc++-7
+sudo apt-get install --yes      \
+        clang-8                 \
+        llvm-8-dev              \
+        gcc-7                   \
+        g++-7
+
+sudo update-alternatives --install /usr/bin/gcc gcc `which gcc-7` 10
+sudo update-alternatives --install /usr/bin/g++ g++ `which g++-7` 10
+sudo update-alternatives --set cc /usr/bin/gcc
+sudo update-alternatives --set c++ /usr/bin/g++
 
